f3e36964369760eca488c18826d61534e9848ba9
Hetpatel <160214289+hetpatelhari17@users.noreply.github.com>
2024-02-21 18:21:24 +0530

    Add files via upload

---
+++ Controllers/Home1Controller.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECommerceShoppingCartAppASPNET7.Areas.Identity.Data;
+using ECommerceShoppingCartAppASPNET7.Models;
+
+namespace ECommerceShoppingCartAppASPNET7.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        private readonly ECommerceIdentityContext _context;
+
+        public Home1Controller(ECommerceIdentityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Home1
+        public async Task<IActionResult> Index()
+        {
+              return _context.Product != null ? 
+                          View(await _context.Product.ToListAsync()) :
+                          Problem("Entity set 'ECommerceIdentityContext.Product'  is null.");
+        }
+
+        // GET: Home1/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Home1/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Photo,Quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Home1/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Home1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Price,Photo,Quantity")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Home1/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Home1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'ECommerceIdentityContext.Product'  is null.");
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(string id)
+        {
+          return (_context.Product?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/_productService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ECommerceShoppingCartAppASPNET7.Controllers
+{
+    internal class _productService
+    {
+        internal static async Task<string?> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
